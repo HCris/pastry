56c2a3d8d7708e00b11a8017df04228a9f481c1f
HCris <cristina.haruta95@gmail.com>
2017-07-13 19:21:16 +0300

    inceput

---
+++ pastry.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+		include("component/meta.html");
+		include("component/link.php");
+		include("component/script.php");
+	?>
+</head>
+<body>
+	<div class="hero"></div>
+	<?php
+		include("component/navbar.html");
+	 ?>
+
+</body>
+</html>
