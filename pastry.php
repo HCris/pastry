54a137647a549287cb02c2ed9cb6dcc5bb13ed56
HCris <cristina.haruta95@gmail.com>
2017-07-17 23:14:02 +0300

    17.07.2017

--- pastry.php
+++ pastry.php
@@ -8,10 +8,62 @@
 	?>
 </head>
 <body>
-	<div class="hero"></div>
-	<?php
-		include("component/navbar.html");
-	 ?>
+	<div class="row">
+			<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<header>
+					<div class="nav">
+						<ul>
+							<li>
+								<a href=""></a>
+							</li>
+						</ul>
+					</div>
+				<div class="container">
+					<p>drkuyjfui</p>
+				</div>
+				</header>
+			</div>
+	</div>
+
+		<div class="row">
+			<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<section>
+					<div class="container">
+						<p>drkuyjfui</p>
+					</div>
+				</section>
+			</div>
+		</div>
+
+	<div class="row">>
+		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<section>
+				<div class="container">
+					<p>drkuyjfui</p>
+				</div>
+			</section>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<section>
+				<div class="container">
+					<p>drkuyjfui</p>
+				</div>
+			</section>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<footer>
+				<div class="container">
+					<p>drkuyjfui</p>
+				</div>
+			</footer>
+		</div>
+	</div>
 
 </body>
 </html>
