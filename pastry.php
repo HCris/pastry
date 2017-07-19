62745a554211b13d74bd705a80d40609dd72fbbf
HCris <cristina.haruta95@gmail.com>
2017-07-19 11:46:50 +0300

    ora 11:46

--- pastry.php
+++ pastry.php
@@ -1,73 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<?php
-		include("component/meta.html");
-		include("component/link.php");
-		include("component/script.php");
-	?>
-</head>
-<body>
-	<div class="row">
-			<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<header>
+	<head>
+		<?php
+			include("component/meta.html");
+			include("component/link.php");
+			include("component/script.php");
+		?>
+	</head>
+	<body>
+		<header>
+			<div class="row">
+				<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
 					<div class="nav">
 						<ul>
 							<li>
 								<a href=""></a>
 							</li>
 						</ul>
-					</div>
-				<div class="container">
-					<h1 class="title">Pastry with love</h1><br>
-					<img class="line" src="img/line.png" alt="">
-					<img class="dashed_line" src="img/dashed line.png" alt="">
-					<p class="message">After downloading everything is ready to start.The folder you have downloaded contains both core bootstrap files and our framework files. Everything is linked and ready to use. Just open the file index.html and start to create some amazing thing!</p>
-
+					<div>
 				</div>
-				</header>
 			</div>
-	</div>
 
-		<div class="row">
-			<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<section>
-					<div class="container">
+			<div class="row">
+				<div class=" hero col col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+					<div class="top">
+						<h1 class="title text-center">Pastry with love</h1><br>
+						<img class="line" src="img/line.png" alt=""><br>
+						<img class="dashed_line" src="img/dashed line.png" alt=""><br>
+						<p class="message ">We're briging you fresh ingredients every</p><br>
+							<p class="message2">day in ways you can't resist</p>
+						<button class="btn ">our meneu</button>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<section>
+			<div class="row">
+				<div class="col ">
 						<p>drkuyjfui</p>
-					</div>
-				</section>
+				</div>
 			</div>
-		</div>
+		</section>
 
-	<div class="row">>
-		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<section>
-				<div class="container">
-					<p>drkuyjfui</p>
+		<section>
+			<div class="row">
+				<div class="col">
+						<p>drkuyjfui</p>
 				</div>
-			</section>
-		</div>
-	</div>
+			</div>
+		</section>
 
-	<div class="row">
-		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<section>
-				<div class="container">
-					<p>drkuyjfui</p>
+		<section>
+			<div class="row">
+				<div class="col">
+						<p>drkuyjfui</p>
 				</div>
-			</section>
-		</div>
-	</div>
+			</div>
+		</section>
 
-	<div class="row">
-		<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<footer>
-				<div class="container">
-					<p>drkuyjfui</p>
+		<footer>
+			<div class="row">
+				<div class="col">
+						<p>drkuyjfui</p>
 				</div>
-			</footer>
-		</div>
-	</div>
+			</div>
+		</footer>
 
-</body>
+	</body>
 </html>
