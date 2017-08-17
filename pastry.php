8cbf1c2f7a70b9f263b028464bd417e679d65fa5
HCris <cristina.haruta95@gmail.com>
2017-08-17 16:21:50 +0300

    revaliution

--- pastry.php
+++ pastry.php
@@ -5,67 +5,162 @@
 			include("component/meta.html");
 			include("component/link.php");
 			include("component/script.php");
+			'<link href="https://fonts.googleapis.com/css?family=Alegreya|Bellefair" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">'
 		?>
+	 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">>
 	</head>
 	<body>
-		<header>
-			<div class="row">
-				<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-					<div class="nav">
-						<ul>
-							<li>
-								<a href=""></a>
-							</li>
-						</ul>
-					<div>
+		<!-- section 1-->
+			<header>
+				<img class="bg_shadow" src="img/bg_shadow.png">
+				<div class="row" style="margin-right:0">
+					<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="nav">
+							<ul>
+								<li>
+									<a href=""></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+<<<<<<< HEAD
 				</div>
-			</div>
 
-			<div class="row">
-				<div class=" hero col col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
-					<div class="top">
-						<h1 class="title text-center">Pastry with love</h1><br>
-						<img class="line" src="img/line.png" alt=""><br>
-						<img class="dashed_line" src="img/dashed line.png" alt=""><br>
-						<p class="message ">We're briging you fresh ingredients every</p><br>
-							<p class="message2">day in ways you can't resist</p>
-						<button class="btn ">our meneu</button>
+				<div class="row" style="margin-right:0">
+					<div class="col col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+						<div class="top">
+							<h1 class="title text-center">Pastry with love</h1><br>
+							<img class="line" src="img/line.png" alt=""><br>
+							<img class="dashed_line" src="img/dashed line.png" alt=""><br>
+							<p class="message ">We're briging you fresh ingredients every<br>
+							day in ways you can't resist</p>
+							<button class="btn ">our meneu</button>
+						</div>
+					</div>
+=======
+				<div class="container">
+					<p>drkuyjfui</p>
+>>>>>>> parent of 75842fd... frwget
+				</div>
+
+				<div class="divider">
+					<img src="img/divider.png">
+				</div>
+			</header>
+		<!-- end section 1-->
+
+		<!-- section 2-->
+			<section class="art_cakes">
+				<div class="row" style="margin-right:0">
+					<div class="col col-xs-10 col-sm-10  col-md-8  col-lg-6 left_content">
+						<h1  class="Art_of_cakes">Art of cakes <br></h1>
+						<h3 class="we_create_delicious_memories">we create delicious memories</h3>
+						<p class="Quisque_rutrum">
+							Quisque rutrum. Aenean mperdiet. Etiam ultricies nisi vel augue.<br>
+							Curabitur ullamcorper ultrices nisi. Nam eget dui. Etiam rhoncus <br>
+							Marcenas tempus, tellus eget condimendume rhoncus, sem quam <br>
+							semper libero, sit amet adipicing sem neque sed
+						</p>
+						<h3 class="Chef_Cook">Chef Cook</h3>
+
+						<div class="avatar">
+							<div class="box1">
+								<img class="line" src="img/line1.png" alt="">
+								<h4>Benito</h4>
+								<img class="line" src="img/line1.png" alt="">
+							</div>
+								<img class="Ellipse" src="img/Ellipse 2.png" alt="">
+							<div class="box2">
+								<img  class="line" src="img/line1.png" alt="">
+								<h4>Gaspare</h4>
+								<img class="line" src="img/line1.png" alt="">
+							</div>
+						</div>
+						<div class="quote">
+							<div class="ghilimele">
+								&ldquo;
+							</div>
+								<h6> Unique creation for unique <br>occasions &rdquo;</h6>
+							<div class="ghilimele_1">
+								&rdquo;
+							</div>
+						</div>
+					</div>
+					<!-- end left_content-->
+
+					<!-- right_content-->
+					<div class=" col col-xs-10 col-sm-10  col-md-6 col-lg-6 img_left_top">
+						<div class="vector">
+							<h3 class="TASTES_SO__GOOD_">TASTES SO GOOD!</h3>
+						</div>
+						<div class="bgi1">
+							<img class="border" src="img/border.png" alt="">
+						</div>
+						<div class="bgi2">
+							<img class="border" src="img/border.png" alt="">
+						</div>
+						<div class="bgi3">
+							<img class="border" src="img/border.png" alt="">
+						</div>
+						<div class="bgi4">
+							<img class="border" src="img/border.png" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+		<!-- end section-->
+
+		<section class="section3">
+			<div class="row" style="margin-right:0">
+				<div class="col col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 widget">
+					<div class="widget_bg">
+						<!-- RATING - Form -->
+						<i class="fa fa-star" aria-hidden="true"></i><br>
+						<i class="fa fa-star" aria-hidden="true"></i><br>
+						<i class="fa fa-star" aria-hidden="true"></i><br>
+						<i class="fa fa-star" aria-hidden="true"></i><br>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+					</div>
+					<img class="menu" src="img/section3/menu.png" alt="">
+					<div class="text_bg">
+						<img src="img/section3/text_bg.png" alt="">
+						<h1>Tasty pancakes</h1>
+						<h4> SEASON FAVOURITE</h4>
+						<p>
+							Timp de câteva zile, poliţiştii de patrulare au verificat cum îşi desfăşoară activitatea zilnică a taximetriştilor.
+						</p>
+						<div class="i">
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+						</div>
+						<div class="ready">
+						jdc;oiwd[]
+						</div>
 					</div>
 				</div>
 			</div>
-		</header>
-
-		<section>
-			<div class="row">
-				<div class="col ">
-						<p>drkuyjfui</p>
-				</div>
-			</div>
 		</section>
 
-		<section>
-			<div class="row">
+		<section class="section4">
+			<div class="row" style="margin-right:0">
 				<div class="col">
 						<p>drkuyjfui</p>
 				</div>
 			</div>
 		</section>
 
-		<section>
-			<div class="row">
+		<footer class="section5">
+			<div class="row" style="margin-right:0">
 				<div class="col">
+				<img src="img/Vektor.png" alt="">
 						<p>drkuyjfui</p>
 				</div>
 			</div>
-		</section>
-
-		<footer>
-			<div class="row">
-				<div class="col">
-						<p>drkuyjfui</p>
-				</div>
-			</div>
-		</footer>
+		</footer> 
 
 	</body>
 </html>
